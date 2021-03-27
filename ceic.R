b594fc8afb2714eebe3197fbeb07edda99b297de
zemw <19867783+zemw@users.noreply.github.com>
2021-03-28 01:45:37 +1100

    Refactor CEIC parser

---
+++ ceic.R
@@ -0,0 +1,115 @@
+library(tidyverse)
+library(magrittr)
+library(xts)
+
+source('regex.R')
+
+
+ceic_load <- function(file) {
+  
+  .raw_text <- read_file(file) # load matlab .m file
+  
+  .series <- NULL   # store time series data
+  .metadata <- NULL # store metadata of the series
+  
+  re_data <- "(?<var>\\w*)\\s=\\stimeseries\\(\\[(?<value>.*?)],\\s\\{(?<index>.*?)\\}\\);"
+  re_name <- "(?<var>\\w*)\\.Name\\s=\\s'(?<name>.*?)';"
+  re_format <- "(?<var>\\w*)\\.TimeInfo\\.Format\\s=\\s'(?<format>.*?)';"
+  re_info <- "(?<var>\\w*)\\.UserData\\.seriesInfo\\s=\\sstruct\\((?<info>.*?)\\);"
+  re_key_value <- "'(?<key>\\w*)',(?<value>'.*?'|\\[.*?\\])"
+  
+  m_data <- re_matches(.raw_text, re_data) 
+  m_name <- re_matches(.raw_text, re_name) 
+  m_info <- re_matches(.raw_text, re_info) 
+  m_format <- re_matches(.raw_text, re_format) 
+  
+  # extract metadata (key-value pairs) of a series from seriesInfo, and 
+  # join the metadata of all series into one table.
+  .metadata <-
+    m_info %>% pmap(function(var, info) {
+      re_matches(info, re_key_value) %>%
+        mutate_if(is.character, ~ str_remove_all(., "\'")) %>%
+        filter(key != "") %>%
+        pivot_wider(names_from = key) 
+    }) %>% reduce( ~ bind_rows(.x, .y)) 
+  
+  # extract times series dates and values from text
+  # return a list of xts objects
+  .series <-
+    m_data %>% pmap(function(var, value, index) {
+      values <- str_split(value, ';')[[1]]
+      dates <- str_split(index, ',')[[1]]
+      xts(as.numeric(values), lubridate::mdy(dates))
+    })
+  names(.series) <- .metadata$seriesId
+  
+  
+  ## return a list of functions
+  list(
+    
+    # query metadata of series by keywords in names
+    query = function(keyword = NULL) {
+      if (!is.null(keyword)) {
+        .metadata %>%
+          filter(str_detect(seriesName, fixed(keyword, ignore_case = T)))
+      } else {
+        .metadata
+      }
+    }, 
+    
+    # fetch time series by id or name
+    fetch = function(seriesId = NULL, 
+                     keyword = NULL, 
+                     output = c('df', 'xts'), 
+                     rename_cols = NULL) {
+      
+      x <- NULL # series to be returned
+      
+      if (!is.null(seriesId)) {
+        x <- .series[seriesId] %>% reduce(~ merge(.x, .y))
+      } else if (!is.null(keyword)) {
+        seriesId <- ceic_query(keyword) %>% pull(seriesId)
+        x <- .series[seriesId] %>% reduce(~ merge(.x, .y))
+      } else {
+        stop('No series IDs or names specified!')
+      }
+      
+      if (is.null(x)) return(x) # no series found
+      
+      if (!is.null(rename_cols)) {
+        names(x) <- rename_cols
+      } else {
+        names(x) <- seriesId
+      }
+      
+      output <- match.arg(output)
+      if (output == 'df') {
+        data.frame(index=index(x), x) %>% as_tibble
+      } else {
+        x
+      }
+    },
+    
+    # translate series id into names
+    get_name = function(series_id, with_unit = FALSE) {
+      name <- .metadata %>% 
+        filter(seriesId == series_id) %>%
+        pull(seriesName)
+      if (with_unit) {
+        unit <- .metadata %>% 
+          filter(seriesId == series_id) %>%
+          pull(Unit)
+        sprintf("%s (%s)", name, unit)
+      } else {
+        name
+      }
+    }
+    
+  ) # end of list
+}
+
+
+cki_m <- ceic_load("CKI_M.m")
+cki_q <- ceic_load("CKI_Q.m")
+
+
