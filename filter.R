9086c36dd106892c797466b29768a44a18426343
zemw <19867783+zemw@users.noreply.github.com>
2021-03-31 17:26:27 +1100

    Add serialisation method

--- filter.R
+++ filter.R
@@ -1,28 +1,12 @@
 library(tidyverse)
 library(lubridate)
+library(magrittr)
 library(seasonal)
 library(mFilter)
 library(timetk)
 library(rlang)
 
-
-tbl_to_xts <- function(object, index_var, data_var) {
-  
-  stopifnot(!missing(object) && is_tibble(object))
-  stopifnot(!missing(index_var), !missing(data_var))
-  
-  index_var <- enquo(index_var)
-  data_var <- enquo(data_var)
-  
-  index <- object %>% pull(!!index_var)
-  data <- object %>% pull(!!data_var)
-  
-  x <- xts(x = data, order.by = index)
-  names(x) <- quo_name(data_var)
-  
-  x
-}
-
+# X11 seasonal adjustment
 x11_adjust <- function(x, index = NULL) {
   
   if (is.zoo(x)) index <- index(x)
@@ -49,6 +33,7 @@ x11_adjust <- function(x, index = NULL) {
   )
 }
 
+# Hodrick-Prescott filter
 hp_filter <- function(x, .lambda=1600) {
   # returns an mFilter object
   result <- hpfilter(x, freq = .lambda, type = "lambda")
@@ -56,11 +41,83 @@ hp_filter <- function(x, .lambda=1600) {
   unclass(result) 
 }
 
-cf_filter <- function(x, .pl, .pu, .root=TRUE, .drift=TRUE) {
+# Christiano-Fitzgerald filter
+cf_filter <- function(x, .pl, .pu, .root=T, .drift=T) {
+  # returns an mFilter object
   result <- cffilter(x, 
                      pl=.pl, 
                      pu=.pu, 
                      root=.root, 
                      drift=.drift)
   unclass(result) 
+}
+
+
+.tbl_to_xts <- function(object, index_var, data_var) {
+  
+  stopifnot(!missing(object) && is_tibble(object))
+  stopifnot(!missing(index_var), !missing(data_var))
+  
+  index_var <- enquo(index_var)
+  data_var <- enquo(data_var)
+  
+  index <- object %>% pull(!!index_var)
+  data <- object %>% pull(!!data_var)
+  
+  x <- xts(x = data, order.by = index)
+  names(x) <- quo_name(data_var)
+  
+  x
+}
+
+# series a time series into a string of Matlab code
+# seriesInfo = 
+#   list(
+#     seriesId = 'CRDQCNAPABIS',
+#     srCode = 'SRCRDQCNAPABIS',
+#     country = 'China',
+#     seriesName = 'CN: Total Credit to Private Non-Financial Sector',
+#     nameLocal = '',
+#     Unit = 'RMB bn',
+#     frequency = 'Quarterly',
+#     source = 'Bank for International Settlements',
+#     firstObsDate = '10/01/1985 00:00:00',
+#     LastObsDate = '',
+#     multiplierCode = 'BN',
+#     tLastUpdTime = '',
+#     seriesStatus = '',
+#     remarks  = '',
+#     functionInformation = '""'
+#   )
+.ts_serialize <- function(object, index_var, data_var, 
+                          name = "", timeFormat = "",
+                          seriesInfo = list()) {
+  
+  index_var <- enquo(index_var)
+  data_var <- enquo(data_var)
+  var_name <- quo_name(data_var)
+  
+  index <- object %>% pull(!!index_var)
+  data <- object %>% pull(!!data_var)
+  
+  str_data <- sprintf("%s =\ntimeseries([%s], {%s});", 
+                      var_name,
+                      paste(data, collapse = ";"), 
+                      paste(
+                        index %>% 
+                          map_chr(~format.Date(., "%m/%d/%Y")) %>%
+                          map_chr(~sprintf("'%s'", .)), 
+                        collapse = ","))
+  
+  str_name <- str_c(sprintf("\n%s.Name = '%s';", var_name, name), 
+                    sprintf("\n%s.TimeInfo.Format = '%s';", var_name, timeFormat))
+  
+  str_info <- sprintf("\n%s.UserData.seriesInfo = struct(%s);", 
+                      var_name, 
+                      map2_chr(seriesInfo, 
+                               names(seriesInfo), 
+                               ~ sprintf("'%s','%s'", .y, .x)) %>%
+                        str_c(collapse = ","))
+  
+  str_c(str_data, str_name, str_info)
 }
